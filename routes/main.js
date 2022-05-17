61a13ad5cf51b66ce18c2b642733d331663403c8
Shawn Koski <skoski@artnet.com>
2022-05-17 00:06:52 -0400

    fetch projects

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,34 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const fetch = require('node-fetch');
 
 router.get('/', function (req, res) {
   res.render('pages/index');
+});
+
+router.get('/projects/:page', async function (req, res, next) {
+  const perPage = 9;
+  const page = req.params.page || 1;
+
+  try {
+    const uri = `http://api.hackaday.io/v1/projects?api_key=fFndNxfzWpFivelu&page=${page}&per_page=${perPage}`;
+
+    const response = await fetch(uri, {
+      method: 'GET',
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' }
+    }).then((res) => res.json());
+
+    console.log('res', response);
+
+    res.render('pages/projects', {
+      current: response.page,
+      pages: response.last_page,
+      projects: response.projects
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 
 const app = express();
