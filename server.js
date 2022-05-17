e55512a1dcbcb3016ba790987826df4212a96fe5
Shawn Koski <skoski@artnet.com>
2022-05-16 23:14:42 -0400

    basic server and test route

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+const mainRoutes = require('./routes/main');
+app.use(mainRoutes);
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
