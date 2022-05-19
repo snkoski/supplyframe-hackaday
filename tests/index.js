a20f218a652c1df8832e606cce353d52684b908a
Shawn Koski <skoski@artnet.com>
2022-05-19 00:53:17 -0400

    start projects page test

--- tests/index.js
+++ tests/index.js
@@ -6,5 +6,14 @@ module.exports = {
       .assert.title('Hackaday')
       .assert.containsText('h1', 'Hackaday Projects Go Here')
       .end();
+  },
+  'Check projects page': function (client) {
+    client
+      .url('http://localhost:3000/projects')
+      .waitForElementVisible('body', 15000)
+      .assert.title('Hackaday')
+      .assert.containsText('h1', 'Select a Project')
+      .assert.containsText('h3', 'Hackaday.io Project')
+      .end();
   }
 };
