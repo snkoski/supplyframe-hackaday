bdcf0ab939d797e150a5bf396d0539028f4d8f91
Shawn Koski <skoski@artnet.com>
2022-05-19 21:45:20 -0400

    more tests

--- tests/index.js
+++ tests/index.js
@@ -4,7 +4,7 @@ module.exports = {
       .url('http://localhost:3000/')
       .waitForElementVisible('body', 1000)
       .assert.title('Hackaday')
-      .assert.containsText('h1', 'Hackaday Projects Go Here')
+      .assert.containsText('h1', 'Go to projects page')
       .end();
   },
   'Check projects page': function (client) {
@@ -13,7 +13,25 @@ module.exports = {
       .waitForElementVisible('body', 15000)
       .assert.title('Hackaday')
       .assert.containsText('h1', 'Select a Project')
-      .assert.containsText('h3', 'Hackaday.io Project')
+      .assert.containsText('h2', 'Hackaday.io Project')
+      .end();
+  },
+  'Check 2nd projects page': function (client) {
+    client
+      .url('http://localhost:3000/projects?page=2')
+      .waitForElementVisible('body', 15000)
+      .assert.title('Hackaday')
+      .assert.containsText('h1', 'Select a Project')
+      .assert.containsText('h2', 'ramanPi - Raman Spectrometer')
+      .end();
+  },
+  'Check project #46 page': function (client) {
+    client
+      .url('http://localhost:3000/projects/46')
+      .waitForElementVisible('body', 15000)
+      .assert.title('Hackaday')
+      .assert.containsText('h1', 'Hackaday.io Project')
+      .assert.containsText('p', 'Summary:')
       .end();
   }
 };
